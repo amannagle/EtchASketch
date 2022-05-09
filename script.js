b7b4f41ebb51f3c481af99e08beec94fd6e0c93c
aman <aman.nagle1999@gmail.com>
2022-05-09 21:08:45 +0530

    adjusted css to reflect adjusted grid size

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
 const divcontainer = document.querySelector('.divcontainer');
-for(let i=1;i<257;i++)
+let x=16;
+makegrid(16);
+function makegrid(x)
 {
-    console.log('looopin')
+    let squares=x*x;
+    //clearing old divs
+    while(divcontainer.firstChild)
+    {
+        divcontainer.removeChild(divcontainer.lastChild);
+    }
+    for(let i=0;i<squares;i++)
+    {
     const div = document.createElement('div');
     divcontainer.appendChild(div);
+    }
+    let percentage=100/x;
+    let div_height=400/x;
+    const divs=document.querySelectorAll('.divcontainer div');
+    divs.forEach(div=>{
+        div.style.flex=`1 0 ${percentage}%`
+        div.style.height=`${div_height}px`
+    })
 }
 function hoverDivs(e)
 {
     const hover_div = e.target;
-    console.log(hover_div);
     hover_div.style.backgroundColor = 'blue';
 }
 divcontainer.addEventListener('mouseover',hoverDivs)
+
+const grid = document.querySelector('#grid_button');
+grid.addEventListener('click',changeGridSize)
+
+function changeGridSize(e)
+{
+    x= parseInt(prompt("Please Enter the Grid Size you want",""),10)
+    if(isNaN(x))
+        alert('invalid input !. Please enter a number between 1 and 100');
+    else if(x<=0 || x>100)
+    {
+        alert('invalid input !. Please enter a number between 1 and 100')
+    }
+    else
+    makegrid(x);
+}
